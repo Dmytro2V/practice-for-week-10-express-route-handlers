734ae945685f95dd1e98e79f556513ce8426479b
Dmytro2V <op2vin@gmail.com>
2023-03-02 13:40:57 +0200

    done (phase1-2, test1-2)

--- server/app.js
+++ server/app.js
@@ -25,6 +25,27 @@ const express = require('express');
 const app = express();
 
 // Your code here
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Body:', req.body);
+  next();
+});
+//Get all the artists
+app.get('/artists', (req, res) => {
+  res.send(getAllArtists());
+})
+//Add an artist
+app.post('/artists', (req, res) => {
+  let newArtist = addArtist(req.body);
+  res.status(201).send(newArtist);
+})
+
+
+
+
+
+
+
 
 // DO NOT MODIFY
 if (require.main === module) {
